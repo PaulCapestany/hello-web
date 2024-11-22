2059d3b0da73a38d40ecd227a0d170ec2ea729ae
Paul Capestany <capestany@gmail.com>
2024-11-22 19:07:36 -0400

    Please work?

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Hello, for the love of zeus please work!!!!!!!!!!!!!!!!!!!!!!!!!!")
+	fmt.Fprintln(w, "Hello, for the love of zeus, tell me this works now?")
 }
 
 func main() {
